b283037a7ecb4fd66e44511703dc81fc728c8dc5
anikamorris <anikamorris@students.berkeley.net>
2017-06-29 14:23:42 -0700

    created login UI

---
+++ Makestagram/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  Makestagram
+//
+//  Created by Anika Morris on 6/29/17.
+//  Copyright © 2017 Anika Morris. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
