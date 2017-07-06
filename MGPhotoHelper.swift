df83fd403da8f0c629f3ca6db8779240afab712a
anikamorris <anikamorris@students.berkeley.net>
2017-07-06 13:11:09 -0700

    functioning photo taking abilities

--- MGPhotoHelper.swift
+++ MGPhotoHelper.swift
@@ -44,11 +44,24 @@ class MGPhotoHelper: NSObject {
     func presentImagePickerController(with sourceType: UIImagePickerControllerSourceType, from viewController: UIViewController) {
         let imagePickerController = UIImagePickerController()
         imagePickerController.sourceType = sourceType
+        imagePickerController.delegate = self
         
         viewController.present(imagePickerController, animated: true)
     }
 }
 
-
+extension MGPhotoHelper: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            completionHandler?(selectedImage)
+        }
+        
+        picker.dismiss(animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
+    }
+}
 
 
