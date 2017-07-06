d64bf8a57ab34b53c455d7d21b572933fd2c2b78
anikamorris <anikamorris@students.berkeley.net>
2017-07-06 15:02:47 -0700

    initialized variables in Post.swift

---
+++ Makestagram/Post.swift
@@ -0,0 +1,25 @@
+//
+//  Post.swift
+//  Makestagram
+//
+//  Created by Anika Morris on 7/6/17.
+//  Copyright © 2017 Anika Morris. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import FirebaseDatabase.FIRDataSnapshot
+
+class Post {
+    var key: String?
+    let imageURL: String
+    let imageHeight: CGFloat
+    let creationDate: Date
+    
+    init(imageURL: String, imageHeight: CGFloat) {
+        self.imageURL = imageURL
+        self.imageHeight = imageHeight
+        self.creationDate = Date()
+    }
+    
+}
