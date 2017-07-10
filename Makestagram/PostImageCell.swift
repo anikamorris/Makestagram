1fda72c1ad45de63025e8d472ae536fc016f3f40
anikamorris <anikamorris@students.berkeley.net>
2017-07-10 10:38:53 -0700

    kingfisher installed and PostImageCell created

---
+++ Makestagram/PostImageCell.swift
@@ -0,0 +1,18 @@
+//
+//  PostImageCell.swift
+//  Makestagram
+//
+//  Created by Anika Morris on 7/10/17.
+//  Copyright © 2017 Anika Morris. All rights reserved.
+//
+
+import UIKit
+
+class PostImageCell: UITableViewCell {
+    
+    @IBOutlet weak var postImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
